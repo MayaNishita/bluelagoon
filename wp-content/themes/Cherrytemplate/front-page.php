7bbc42c0fd2f3a0cf24e41bdd8106ec221f4012a
MayaNishita <mayaxxx1101@gmail.com>
2018-04-19 00:28:19 +0900

    サブループタグ修正

--- wp-content/themes/Cherrytemplate/front-page.php
+++ wp-content/themes/Cherrytemplate/front-page.php
@@ -13,29 +13,28 @@ get_header(); ?>
 <main>
 	   <section id="area1" class="clearfix">
         <h2><span>Service</span></h2>
+       <?php query_posts( 'category_name=service&posts_per_page=3' ); ?>
+       <?php while ( have_posts() ) : the_post(); ?>
         <article class="floatnone view view-first">
-                <?php query_posts( 'category_name=service&posts_per_page=3' ); ?>
-                    <?php while ( have_posts() ) : the_post(); ?>
                      <div class="view view-first"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium' ); ?></a></div>
                      <div class="mask">
                         <h3><?php echo get_the_title(); ?></h3>
                         <a href="<?php the_permalink(); ?>">Read More</a>
-                     <?php endwhile; ?>
                  </div>
-             </article>
+        </article>
+        <?php endwhile; ?>
         </section>
         <section id="area2" class="clearfix">
-            <h2><span>Blog</span></h2>
-            <article class="floatnone view view-first">
-                    <?php query_posts( 'category_name=blog&posts_per_page=3' ); ?>
-                        <?php while ( have_posts() ) : the_post(); ?>
-                         <div class="view view-first"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium' ); ?></a></div>
-                         <div class="mask">
-                            <h3><?php echo get_the_title(); ?></h3>
-                            <a href="<?php the_permalink(); ?>">Read More</a>
-                         <?php endwhile; ?>
-                     </div>
-                 </article>
+       <?php query_posts( 'category_name=blog&posts_per_page=3' ); ?>
+       <?php while ( have_posts() ) : the_post(); ?>
+        <article class="floatnone view view-first">
+                     <div class="view view-first"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium' ); ?></a></div>
+                     <div class="mask">
+                        <h3><?php echo get_the_title(); ?></h3>
+                        <a href="<?php the_permalink(); ?>">Read More</a>
+                 </div>
+        </article>
+        <?php endwhile; ?>
         </section>
     <div id="page-top" class="page-top">
         <p><a id="move-page-top" class="move-page-top">▲</a></p>
--- wp-content/themes/Cherrytemplate/functions.php
+++ wp-content/themes/Cherrytemplate/functions.php
@@ -27,3 +27,4 @@ register_sidebar(array(
      'before_title' => '<h3>',
      'after_title' => '</h3>'
 ));
+add_theme_support( 'post-thumbnails' );
